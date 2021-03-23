643a6886f2b6089c19635b13b4084b7a63e9197c
arai <kensuke.y.arai@gmail.com>
2021-03-23 05:17:14 -0400

    practice mode

--- rpsm.js
+++ rpsm.js
@@ -63,86 +63,117 @@ var machines = [__MC__, __MC2__, __PRC__, __RND__];
 var realtimeResultsInfo = [__NGAMES__, __CWTL__];
 
 class MarkovChain {
-
-	constructor(decay) {
-		this.decay = decay;
-		this.matrix = {
-			"11":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
-			"12":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
-			"13":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
-			"21":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
-			"22":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
-			"23":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
-			"31":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
-			"32":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
-			"33":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}}
-		}
+    constructor(decay) {
+	this.decay = decay;
+	this.matrix = {
+	    "11":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
+	    "12":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
+	    "13":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
+	    "21":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
+	    "22":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
+	    "23":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
+	    "31":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
+	    "32":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}},
+	    "33":{"1":{"prob":1/3, "n_obs":0}, "2":{"prob":1/3, "n_obs":0}, "3":{"prob":1/3, "n_obs":0}}
 	}
+	this.internal_state = [];
+	var prno_9x6 = this.to_9x6s(this.internal_state);
+	this.internal_state[0] = prno_9x6;
+    }
 
-	update_matrix(pair, input) {
-
-		for (var i = 1; i < 4; i++) {
-			this.matrix[pair][String(i)]['n_obs'] = this.decay * this.matrix[pair][String(i)]['n_obs'];
-		}
-
-		this.matrix[pair][input]['n_obs']++;
-
-		var n_total = 0;
-		for (var i = 1; i < 4; i++) { 
-			n_total += this.matrix[pair][String(i)]['n_obs'];
-		}
-
-		for (var i = 1; i < 4; i++) { 
-			this.matrix[pair][String(i)]['prob'] = this.matrix[pair][String(i)]['n_obs'] / n_total;
-		}
-		
-	}
-
-	predict(pair) {
-		var probs = this.matrix[pair];
-		//blk = parseInt(blk, 10);
-		//console.log(blk);
-
-		switch (AImach) { //change to SQ, PC, RD
-			case __MC2__:
-		    //console.log("MC2");
-				//Method: SQUARED
-				var sample = Math.random();
-				var con = Math.pow((probs['1']['prob']), 2) + Math.pow((probs['2']['prob']), 2) + Math.pow((probs['3']['prob']), 2);
-				
-				var r_prob = Math.pow((probs['1']['prob']), 2)/con;
-				var p_prob = Math.pow((probs['2']['prob']), 2)/con;
-				var s_prob = Math.pow((probs['3']['prob']), 2)/con;
-
-				rps_probs = [r_prob, p_prob, s_prob]
-				//console.log(rps_probs);
-
-				if (sample < r_prob) return "1";
-				else if ((sample >= r_prob) && (sample < r_prob + p_prob)) return "2";
-				else return "3";
-
-			case __MC__:
-		    //console.log("MC");
-				//Method: SOFTMAX
-				var sample = Math.random();
-				var r_prob = probs['1']['prob'];
-				var p_prob = probs['2']['prob'];
-				var s_prob = probs['3']['prob'];
-
-				rps_probs = [r_prob, p_prob, s_prob]
-				//console.log(rps_probs);
-				
-				if (sample < r_prob) return "1";
-				if ((sample >= r_prob) && (sample < r_prob + p_prob)) return "2";
-				if (sample >= r_prob + p_prob) return "3";
-				break;
-
-			default:
-
-		}
-
+    to_9x6s()
+    {
+	var prno_9x6 = [[0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0]];
+	
+	var keys = ["11", "12", "13", "21", "22", "23", "31", "32", "33"];
+	var rpss  = ["1", "2", "3"];
+	
+	var ik, irps, key, rps;
+	for( ik = 0; ik < 9; ik++ )
+	{
+	    key = keys[ik];
+	    for( irps  = 0; irps < 3; irps++ )
+	    {
+		rps = rpss[irps];
+    		prno_9x6[ik][irps*2] = this.matrix[key][rps]["prob"];
+    		prno_9x6[ik][irps*2+1] = this.matrix[key][rps]["n_obs"];
+	    }
 	}
 	
+	return prno_9x6;
+    }
+
+    update_matrix(pair, input) {
+	for (var i = 1; i < 4; i++) {
+	    this.matrix[pair][String(i)]['n_obs'] = this.decay * this.matrix[pair][String(i)]['n_obs'];
+	}
+	
+	this.matrix[pair][input]['n_obs']++;
+	
+	var n_total = 0;
+	for (var i = 1; i < 4; i++) { 
+	    n_total += this.matrix[pair][String(i)]['n_obs'];
+	}
+	
+	for (var i = 1; i < 4; i++) { 
+	    this.matrix[pair][String(i)]['prob'] = this.matrix[pair][String(i)]['n_obs'] / n_total;
+	}
+
+	this.internal_state[this.internal_state.length] = this.to_9x6s(this.internal_state);
+	console.log(this.internal_state.length);
+    }
+
+    predict(pair) {
+	var probs = this.matrix[pair];
+	//blk = parseInt(blk, 10);
+	//console.log(blk);
+	
+	switch (AImach) { //change to SQ, PC, RD
+	case __MC2__:
+	    //console.log("MC2");
+	    //Method: SQUARED
+	    var sample = Math.random();
+	    var con = Math.pow((probs['1']['prob']), 2) + Math.pow((probs['2']['prob']), 2) + Math.pow((probs['3']['prob']), 2);
+	    
+	    var r_prob = Math.pow((probs['1']['prob']), 2)/con;
+	    var p_prob = Math.pow((probs['2']['prob']), 2)/con;
+	    var s_prob = Math.pow((probs['3']['prob']), 2)/con;
+	    
+	    rps_probs = [r_prob, p_prob, s_prob]
+	    //console.log(rps_probs);
+	    
+	    if (sample < r_prob) return "1";
+	    else if ((sample >= r_prob) && (sample < r_prob + p_prob)) return "2";
+	    else return "3";
+	    
+	case __MC__:
+	    //console.log("MC");
+	    //Method: SOFTMAX
+	    var sample = Math.random();
+	    var r_prob = probs['1']['prob'];
+	    var p_prob = probs['2']['prob'];
+	    var s_prob = probs['3']['prob'];
+	    
+	    rps_probs = [r_prob, p_prob, s_prob]
+	    //console.log(rps_probs);
+	    
+	    if (sample < r_prob) return "1";
+	    if ((sample >= r_prob) && (sample < r_prob + p_prob)) return "2";
+	    if (sample >= r_prob + p_prob) return "3";
+	    break;
+	    
+	default:
+	    
+	}
+    }
 }
 
 let model = new MarkovChain(0.9);
@@ -445,6 +476,15 @@ function Reset(){
 	predict = 0;
 	pair1 = "";
 	pair2 = "";
+    if (practiceMode)
+    {
+	var bottomDIV = parent.document.getElementById("bottom");
+	bottomDIV.style.visibility = "visible";
+	bottomDIV.style.opacity=0.2;
+	bottomDIV.style.height=200;
+	bottomDIV.style.top=270;
+	bottomDIV.innerHTML='<CENTER><BR><P style="color=#000000;font-size:50px">PRACTICE<BR>MODE</P></CENTER>';
+    }
 }
 
 
@@ -818,7 +858,7 @@ function AI(player){
 
 	//Markov Chain
 	else {
-
+	    console.log("predict is   " + predict);
 		if (predict==0) {
 			var rnd = Math.random(); 
 			if (rnd < 0.333333) predict=1;
@@ -845,6 +885,23 @@ function AI(player){
 	
 }
 
+function goon()
+{
+    //  get out of practice mode
+    MatchTo = real_MatchTo;
+    nextpageURL = callingPage;
+    sessionStorage.setItem("PracticeMode", false);
+    document.location.href=callingPage;
+}
+
+function practiceagain()
+{
+    //  stayin practice mode
+    MatchTo = real_MatchTo;
+    nextpageURL = callingPage;
+    sessionStorage.setItem("PracticeMode", true);
+    document.location.href=callingPage;
+}
 
 function You_win_or_lose(win,lose){
     console.log("You_win_or_lose");
@@ -858,13 +915,13 @@ function You_win_or_lose(win,lose){
     {
 	if (practiceMode)
 	    {
-		var pgo = document.getElementById("practiceorgoon");
+		var pgo = document.getElementById("top");
 
-		pgo.innerHTML = '<img src="done_practicing.jpg" height=30/><BR><img src="more_practice.jpg" height=30/><BR>';
-		pgo.style.backgroundColor="blue";
-		pgo.style.height=100;
+		pgo.innerHTML = '<BR><BR><BR><BR><BR><BR><BR><BR><CENTER><A href="javascript:goon();"><img src="done_practicing.jpg" height=30/></A><BR><BR><BR><A href="javascript:practiceagain();"><img src="more_practice.jpg" height=30/></A></CENTER><BR>';
+		pgo.style.backgroundColor="grey";
+		pgo.style.height=600;
 		pgo.style.width=500;
-		alert("should i be here, yes???");
+		pgo.style.opacity=0.98;
 	    }
 	else
 	    {
@@ -968,14 +1025,8 @@ function You_win_or_lose(win,lose){
     if (!practiceMode)
     {   // don't send 
 	send_php();
+	setTimeout(() => {  location.href=nextpageURL; }, wait_next);
     }
-    else if (practiceMode)
-    {
-	MatchTo = real_MatchTo;
-	nextpageURL = callingPage;
-	sessionStorage.setItem("PracticeMode", false);
-    }
-    setTimeout(() => {  location.href=nextpageURL; }, wait_next);
 }
 
 
