63ab6e48f10c4bbddb5d3738579ac38df7cd9409
arai <kensuke.y.arai@gmail.com>
2021-05-01 07:45:58 -0400

    ruleExp3.html
    Made it so that each AI type is played same number of times.
    
    Also, in different matchups, 1,2 and 2,1  appear same number of times.
    
    11, 22  also appear same number of times.

--- aiconfig3.js
+++ aiconfig3.js
@@ -1,2 +1,3 @@
-var machine_and_configs = ["OutcomeBasedRule(__moRSP__, 3, 0.2, [__DN__, __ST__, __DN__]);", "OutcomeBasedRule(__moRSP__, 3, 0.2, [__UP__, __ST__, __UP__]);"];
-var machine_and_configs_nicknames = ["1", "2"];
+var machine_and_configs_all = ["OutcomeBasedRule(__moRSP__, 3, 0.2, [__ST__, __UP__, __DN__]);", "OutcomeBasedRule(__moRSP__, 3, 0.2, [__ST__, __DN__, __UP__]);", "OutcomeBasedRule(__moRSP__, 3, 0.2, [__DN__, __ST__, __DN__]);", "OutcomeBasedRule(__moRSP__, 3, 0.2, [__UP__, __ST__, __UP__]);"];
+
+var machine_and_configs_nicknames_all = ["1", "2", "3", "4"];
