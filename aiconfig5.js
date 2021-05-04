c2537cedd597a5a8e6f2ecd5f01854584dca2c2c
arai <kensuke.y.arai@gmail.com>
2021-05-04 10:37:34 -0400

    Adding  rulesExp5

--- aiconfig5.js
+++ aiconfig5.js
@@ -1,8 +1,15 @@
 var machines = [["1", "WTL1", "OutcomeBasedRule(__moRSP__, 3, 0.15, [__ST__, __UP__, __DN__]);"], 
-		["2", "MC1", "MarkovChain(0.9, 1);"]]
+		["2", "WTL3", "OutcomeBasedRule(__moRSP__, 3, 0.15, [__DN__, __ST__, __DN__]);"],
+		["3", "Mimic3", "Mimic(__moRSP__, 2, 0.15);"],
+		["4", "WTL2", "OutcomeBasedRule(__moRSP__, 3, 0.15, [__ST__, __DN__, __UP__]);"], 
+		["5", "Mimic1", "Mimic(__moRSP__, 0, 0.15);"], 
+		["6", "WTL4", "OutcomeBasedRule(__moRSP__, 3, 0.15, [__UP__, __ST__, __UP__]);"],
 
-var machines_use_these = [0, 1];
+		["7", "Mimic2", "Mimic(__moRSP__, 1, 0.15);"],
+		["8", "Rnd", "BiasedRandom(__moRSP__, [0.33, 0.33, 0.34])"],
+		["9", "BiasRnd", "BiasedRandom(__moRSP__, [0.25, 0.25, 0.5])"]]
 
+var machines_use_these = [getRandomInt(0, 8), getRandomInt(0, 8)];
 
 var _machine_and_configs = [];
 var _machine_and_configs_indices = [];
